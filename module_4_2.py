024c8ec8ecd3531191a066252a40838adae319fc
Ruslan <ismail96@yandex.ru>
2024-11-28 03:35:48 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -3,3 +3,4 @@ def test_function():
         print('Я в области видимости функции test_function')
     inert_function()
 test_function()
+# inert_function() - NameError: name 'inert_function' is not defined. Did you mean: 'test_function'?
